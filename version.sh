947f8c21365c05ee6f24f7975a2c832b0b426676
Stephen L Arnold <nerdboy@gentoo.org>
2021-12-03 13:27:56 -0800

    create v5.15.x branch for beaglev 5.15.5 patch set
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- version.sh
+++ version.sh
@@ -37,11 +37,11 @@ DEBARCH=riscv64
 toolchain="gcc_11_riscv64"
 
 #Kernel
-KERNEL_REL=5.14
-KERNEL_TAG=${KERNEL_REL}-rc3
+KERNEL_REL=5.15
+KERNEL_TAG=${KERNEL_REL}.5
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
